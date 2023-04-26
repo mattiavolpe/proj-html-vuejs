5dc967ccf05e95a49be19a3fd641af55c20a3f99
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-26 16:10:56 +0200

    ADD presentation component and style

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -5,4 +5,23 @@ export default {
 </script>
 
 <template>
+  <div id="presentation" class="position-relative">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-between">
+        <div class="col-4 text-center">
+          <h2 class="text-start mb-4">
+            Hey there, my name is John Doe. I'm the founder of the Brain Academy.
+          </h2>
+          <img src="src/assets/img/sign.jpg" alt="Sign">
+        </div>
+        <div class="col-7">
+          <p class="m-0">
+            A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquill existence, that I neglect my talents. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /#presentation -->
 </template>
