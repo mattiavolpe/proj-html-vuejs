89e5c93d6fca46f73745774943e7bcb288f3136c
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-26 13:13:59 +0200

    ADD header with icons placeholders

---
+++ src/assets/data/headerMenu.js
@@ -0,0 +1,27 @@
+export const menu = [
+  {
+    name: "Courses",
+    ref: "#",
+    new: false,
+  },
+  {
+    name: "Course Formats",
+    ref: "#",
+    new: false,
+  },
+  {
+    name: "Add Course",
+    ref: "#",
+    new: true,
+  },
+  {
+    name: "Pages",
+    ref: "#",
+    new: false,
+  },
+  {
+    name: "Demos",
+    ref: "#",
+    new: false,
+  },
+]
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,98 @@
 <script>
+import { menu } from "./../assets/data/headerMenu.js"
 export default {
   name: "AppHeader",
+  data() {
+    return {
+      menu,
+      activeMenu: 0,
+    }
+  }
 }
 </script>
 
 <template>
+  <header class="bg-white py-5">
+    <div class="container d-flex justify-content-between align-items-center">
+      <div id="logo">
+        <img src="./../assets/img/MasterStudy-1.svg" alt="Master Study logo">
+      </div>
+      <nav class="d-flex">
+        <ul id="menu" class="d-flex">
+          <li v-for="(item, index) in menu" class="position-relative" @click="activeMenu = index">
+            <div v-if="item.new" class="badge_new">NEW</div>
+            <a class="text-decoration-none" :href="item.ref">{{ item.name }}</a>
+            <div v-if="index === activeMenu" class="active_menu"></div>
+          </li>
+        </ul>
+        <ul id="socials" class="d-flex ms-5">
+          <li>linkedin</li>
+          <li>instagram</li>
+          <li>facebook</li>
+          <li>twitter</li>
+        </ul>
+      </nav>
+    </div>
+    <!-- /.container -->
+  </header>
 </template>
+
+<style lang="scss" scoped>
+@use "./../assets/scss/partials/_variables.scss" as *;
+
+header {
+  #logo > img {
+    height: 45px;
+  }
+
+  nav {
+
+    #menu {
+
+      li:nth-child(even) {
+        margin: 0 1.5rem;
+      }
+
+      .badge_new {
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(0.75rem, -80%);
+        font-size: 0.625rem;
+        font-weight: 700;
+        padding: 0.25rem 0.5rem;
+        color: $lighter;
+        background-color: $badge;
+        border-top-left-radius: 6px;
+        border-top-right-radius: 6px;
+        border-bottom-right-radius: 6px;
+      }
+
+      .active_menu {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        transform: translate(0, 10px);
+        width: 100%;
+        height: 7px;
+        background-color: $accent;
+        clip-path: polygon(0 4px, calc(50% - 4px) 4px, 50% 0, calc(50% + 4px) 4px, 100% 4px, 100% 100%, 0 100%);
+      }
+
+    }
+
+    #socials {
+
+      li:nth-child(even) {
+        margin: 0 0.5rem;
+      }
+
+    }
+
+    a {
+      color: $darker;
+      font-weight: 700;
+    }
+  }
+}
+</style>
