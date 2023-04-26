b12a129f7934a52d5f58c47a2f872476af77cfe9
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-26 18:10:01 +0200

    ADD book component and style

--- src/components/BookComponent.vue
+++ src/components/BookComponent.vue
@@ -5,4 +5,18 @@ export default {
 </script>
 
 <template>
+  <div id="book">
+    <div class="container h-100 d-flex align-items-center">
+      <div class="w-50 text-center">
+        <h2 class="m-0">Get My Exclusive Investing Book for just &dollar;19.50</h2>
+        <em class="my-5">Vestibulum imentum eu urna vel cursus. Aenean behicula quam quam, vitae pellentesque urna elementum cursus.</em>
+        <a href="#" target="_blank">
+          <img src="./../assets/img/amazon.png" alt="Amazon logo">
+        </a>
+      </div>
+      <img class="align-self-end w-50" src="./../assets/img/book.png" alt="Masterstudy Book Template image">
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /#book -->
 </template>
