6c070871eac42e8b2dbd16f531a84625648e5149
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-27 13:48:49 +0200

    FIX presentation component responsiveness

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -8,13 +8,13 @@ export default {
   <section id="presentation" class="position-relative">
     <div class="container h-100">
       <div class="row h-100 align-items-center justify-content-between">
-        <div class="col-xs-4 col-lg-1 text-center">
+        <div class="col-xs-1 col-lg-4 text-center">
           <h2 class="text-start mb-4">
             Hey there, my name is John Doe. I'm the founder of the Brain Academy.
           </h2>
           <img src="./../assets/img/sign.jpg" alt="Sign">
         </div>
-        <div class="col-xs-7 col-lg-1">
+        <div class="col-xs-1 col-lg-7">
           <p>
             A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquill existence, that I neglect my talents. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary
           </p>
