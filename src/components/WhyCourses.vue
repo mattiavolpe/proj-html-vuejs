9e166b257a6e922250d4d8dcfee85756c57a3855
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-27 15:07:25 +0200

    ADD desktop-first responsive why-courses section

--- src/components/WhyCourses.vue
+++ src/components/WhyCourses.vue
@@ -18,7 +18,7 @@ export default {
   <section id="why_courses" class="py-5">
     <div class="container">
       <h2 class="section_title mb-5 text-center">Why my Courses?</h2>
-      <div class="row row-cols-2 g-5">
+      <div class="row row-cols-1 row-cols-md-2 g-5">
         <SingleReason v-for="item in reasons" :key="item.title + item.description" :item="item"></SingleReason>
       </div>
       <!-- /.row row-cols-3 -->
