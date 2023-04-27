19c917661ee2b291ede557bad351b7cfb19a5c95
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-27 12:57:34 +0200

    ADD desktop-first responsive header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,11 @@ export default {
     MenuComponent,
     SocialIcons,
   },
+  data() {
+    return {
+      menuVisible: false,
+    }
+  },
   methods: {
     reloadPage() {
       location.reload();
@@ -17,14 +22,16 @@ export default {
 
 <template>
   <header class="bg-white py-5">
-    <div class="container d-flex justify-content-between align-items-center">
+    <div class="container d-flex justify-content-between align-items-center position-relative">
       <a @click="reloadPage()" id="logo">
         <img src="./../assets/img/MasterStudy-1.svg" alt="Master Study logo">
       </a>
       <!-- /#logo -->
-      <nav class="d-flex">
-        <MenuComponent></MenuComponent>
-        <SocialIcons></SocialIcons>
+      <nav :class="menuVisible ? 'opened' : ''" class="d-flex align-items-center">
+        <MenuComponent v-if="menuVisible"></MenuComponent>
+        <SocialIcons v-if="menuVisible"></SocialIcons>
+        <font-awesome-icon v-if="!menuVisible" icon="fa-solid fa-bars" class="mobile_menu d-none" @click="menuVisible = !menuVisible"/>
+        <font-awesome-icon v-if="menuVisible" icon="fa-solid fa-xmark" class="mobile_menu d-none" @click="menuVisible = !menuVisible"/>
       </nav>
     </div>
     <!-- /.container -->
--- src/main.js
+++ src/main.js
@@ -30,13 +30,15 @@ import {
   faWrench,
   faCartShopping,
   faMobileScreen,
+  faBars,
+  faXmark,
 } from '@fortawesome/free-solid-svg-icons'
 import {
   faClock,
   faThumbsUp,
   faImage,
 } from '@fortawesome/free-regular-svg-icons'
-library.add(faLinkedinIn, faInstagram, faFacebookF, faTwitter, faSignal, faListUl, faClock, faSuitcase, faRocket, faThumbsUp, faGear, faUserGroup, faClapperboard, faImage, faArrowLeft, faArrowRight, faStar, faGithub, faDisplay, faLifeRing, faWrench, faCartShopping, faMobileScreen)
+library.add(faLinkedinIn, faInstagram, faFacebookF, faTwitter, faSignal, faListUl, faClock, faSuitcase, faRocket, faThumbsUp, faGear, faUserGroup, faClapperboard, faImage, faArrowLeft, faArrowRight, faStar, faGithub, faDisplay, faLifeRing, faWrench, faCartShopping, faMobileScreen, faBars, faXmark)
 
 import App from './App.vue'
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
